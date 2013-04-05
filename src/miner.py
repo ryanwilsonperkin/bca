bceb592e8b3db5c0d5e38ef55c6c848ead64fcde
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2013-04-05 16:57:51 -0400

    Secure file opening

--- src/miner.py
+++ src/miner.py
@@ -13,14 +13,15 @@ class Miner:
 		pass
 
 	def load(self, filename):
-		self.blockchain_file = open(filename)	
-		self.blockchain = [line[:-1] if line.endswith('\n') 
-				   else line 
-				   for line in self.blockchain_file]
-		self.current_block = self.blockchain[-1]
+		self.blockchain_file = filename
+		with open(filename, 'r') as f:
+			self.blockchain = [line[:-1] if line.endswith('\n') 
+					   else line 
+					   for line in f]
+			self.current_block = self.blockchain[-1]
 
 	def reload(self):
-		self.load(self.blockchain_file.name)
+		self.load(self.blockchain_file)
 	
 	def mine(self):
 		random.seed(time.clock())
@@ -33,21 +34,27 @@ class Miner:
 			data = "{}{}".format(self.current_block, nonce)
 			hex_hash = hashlib.sha256(data).hexdigest()
 			if int(hex_hash, 16) <= maximum:
-				return hex_hash
+				with open(self.blockchain_file, 'a') as f:
+					f.write(hex_hash)
+					f.write('\n')
+				return
 
 def handler(signum, frame):
 	global miner
-	print "Reloading"
 	miner.reload()
 	
+def finish(signum, frame):
+	exit(0)
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Simplified Bitcoin miner program")
 	parser.add_argument("filename", help="blockchain file")
 	args = parser.parse_args()
-	signal.signal(signal.SIGUSR1, handler)
 	global miner
 	miner = Miner()
 	miner.load(args.filename)
+	signal.signal(signal.SIGINT, finish)
+	signal.signal(signal.SIGUSR1, handler)
 	while True:
-		print miner.mine()
+		miner.mine()
 		miner.reload()
